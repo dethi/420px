e9d91f84528f3054f6c07c4cb5af8232b924b3b8
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-05-04 12:27:36 +0200

    auth: add auth workflow

---
+++ app/Auth.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use App\Models\User;
+
+class Auth
+{
+    private const SESSION_KEY = '420px_user_id';
+    private static $user = null;
+
+    public static function user()
+    {
+        if (self::$user == null) {
+            $id = $_SESSION[self::SESSION_KEY] ?? null;
+            if ($id != null) {
+                self::$user = User::find($id);
+            }
+        }
+
+        return self::$user;
+    }
+
+    public static function check()
+    {
+        return self::user() != null;
+    }
+
+    public static function attempt(string $email, $password)
+    {
+        $user = User::findByEmail($email);
+        if (user == null) {
+            return false;
+        }
+
+        if (!$user->verifyPassword($password)) {
+            return false;
+        }
+
+        $_SESSION[self::SESSION_KEY] = $user->id;
+        return true;
+    }
+}
