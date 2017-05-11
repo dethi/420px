d7f013c003791976cb63e49efb17a088b0bae4ff
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-05-11 15:32:40 +0200

    design: add assets

--- public/fragments/header.php
+++ public/fragments/header.php
@@ -9,6 +9,10 @@ use App\Auth;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>420px</title>
+
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="/assets/css/materialize.min.css"  media="screen,projection"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
 
